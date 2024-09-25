5c03e23640f49a4ad0b46d659912e5047169f528
KohariDev <kohari.dev@gmail.com>
2024-09-25 12:09:03 -0400

    adding changes to github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,15 +1,16 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views  # Import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
+    path(route='login', view=views.login_user, name='login'),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    # path for get cars
+    path(route='get_cars', view=views.get_cars, name='get_cars'),
 
     # path for dealer reviews view
 
