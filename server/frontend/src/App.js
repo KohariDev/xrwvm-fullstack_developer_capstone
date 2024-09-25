ec0dc76a443941313600b53a82f4ef4ec770ddf3
KohariDev <kohari.dev@gmail.com>
2024-09-25 11:20:27 -0400

    Adding changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // Import the Register component
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} /> {/* Add route for the registration page */}
     </Routes>
   );
 }
+
 export default App;
