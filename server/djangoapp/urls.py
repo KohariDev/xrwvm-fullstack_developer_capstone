ba548ea26b8807d55b98e5f9b068d4e86970c14e
KohariDev <kohari.dev@gmail.com>
2024-09-25 12:37:55 -0400

    adding changes to github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,16 @@ urlpatterns = [
     path(route='get_cars', view=views.get_cars, name='get_cars'),
 
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
-    # path for add a review view
+    # Path for fetching dealer details by ID
+    path('dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
+    # Path for fetching reviews of a dealer with sentiment analysis
+    path('reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+
+    # Path for adding a review
+    path('add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
